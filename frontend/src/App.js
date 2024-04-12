b5bdf56119df77ee80115db9b90ee503d9eee27b
CharlesChristianPilapil <charleschristian.pilapil@gmail.com>
2024-04-12 21:39:12 +0800

    routers installed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,20 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import { Outlet } from "react-router-dom";
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
